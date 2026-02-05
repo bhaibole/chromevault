db6e98a0aabb296137be31e7dc60c02f8e3e9dba
bhaibole <luxurystyle8558@gmail.com>
2026-02-05 13:26:23 +0530

    Create auth.js

---
+++ backend/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const SECRET = "CHROMEVault_SECRET";
+
+function createToken(user) {
+  return jwt.sign(
+    { id: user.id, email: user.email, role: user.role },
+    SECRET,
+    { expiresIn: "7d" }
+  );
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, SECRET);
+}
+
+module.exports = { createToken, verifyToken };
